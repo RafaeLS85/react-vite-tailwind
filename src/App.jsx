1e1cb969a16e5cf02cd20bc554175062e403c723
Rafael Olivay <RafaelOlivay.ID@Pinapp.pe>
2022-04-24 00:20:09 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Example from './components/Example'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div>App Works</div>
+      <Example />
+    </>
+  )
+}
+
+export default App
